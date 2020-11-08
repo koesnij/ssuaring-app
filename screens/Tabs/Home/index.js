58b4c8422066b9556c2e48a2313fdabb67ab3acd
Jeon <hamham300@naver.com>
2020-11-09 02:03:37 +0900

    홈화면 리스트뷰 이미지 포함 클릭이벤트

--- screens/Tabs/Home/index.js
+++ screens/Tabs/Home/index.js
@@ -2,45 +2,113 @@ import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
 import styled from 'styled-components';
 import { Header, HeaderLink } from '../../../components/HeaderItem';
-import { RecyclerListView, DataProvider, LayoutProvider } from "recyclerlistview";
+import {View, Text, StyleSheet, FlatList, TouchableOpacity, TouchableWithoutFeedback, Image} from 'react-native';
 
-const View = styled.View`
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-`;
+// const View = styled.View`
+//   justify-content: center;
+//   align-items: center;
+//   flex: 1;
+// `;
 
-const Text = styled.Text``;
+// const Text = styled.Text``;
 
-export default ({ navigation }) => {
-  useEffect(() => {
-    navigation.setParams({ area: '동작구' });
-    console.log('filter:', navigation);
-  }, []);
+const style = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+   logo: {
+    width: 66,
+    height: 58,
+  },
+    itemView:{
+        flexDirection:'row',
+        borderWidth:1,
+        borderRadius:4,
+        padding:8,
+        marginBottom:12,
+    },
+   itemImg:{
+        width:120,
+        height:100,
+        resizeMode:'cover',
+        marginRight:8,
+    },
+    itemName:{
+        fontSize:24,
+        fontWeight:'bold',
+    },
+    itemMsg:{
+        fontSize:16,
+        fontStyle:'italic',
+    },
+});
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitle: '동작구',
-      headerRight: () => {
-        return (
-          <Header>
-            <HeaderLink
-              str={'필터'}
-              onPress={() => navigation.navigate('Filter')}
-            />
-            <HeaderLink
-              str={'지도'}
-              onPress={() => navigation.navigate('Map')}
-            />
-          </Header>
-        );
-      },
-    });
-  }, [navigation]);
 
+//,image: require('./postimg_1')
+const FlatListBasics = () => {
   return (
-    <View>
-      <Text>Home Bitches!</Text>
+    <View style={style.container}>
+        <FlatList
+           data={[
+          {name:"고영욱", message:"널 좋아해",img: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/01/14/52293938.2.jpg"},
+          {name:"신정환", message:"역시 형이야",img:  "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjdfMjgx/MDAxNDkzMjYxNzQyMjU4.op0zQTfA81ih-qf3BEzr_6C2VXoclL_Cbs4aiJiObgkg.EZUQzZsblADVKBE1ZcbbleaRwcyd_KmsUJMwDPWU5GIg.JPEG.mki112112/%EC%8B%A0%EC%A0%95%ED%99%983.jpg?type=w800"},
+            {name:"고영욱", message:"널 좋아해",img: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/01/14/52293938.2.jpg"},
+          {name:"신정환", message:"역시 형이야",img:  "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjdfMjgx/MDAxNDkzMjYxNzQyMjU4.op0zQTfA81ih-qf3BEzr_6C2VXoclL_Cbs4aiJiObgkg.EZUQzZsblADVKBE1ZcbbleaRwcyd_KmsUJMwDPWU5GIg.JPEG.mki112112/%EC%8B%A0%EC%A0%95%ED%99%983.jpg?type=w800"},
+            {name:"고영욱", message:"널 좋아해",img: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/01/14/52293938.2.jpg"},
+          {name:"신정환", message:"역시 형이야",img:  "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjdfMjgx/MDAxNDkzMjYxNzQyMjU4.op0zQTfA81ih-qf3BEzr_6C2VXoclL_Cbs4aiJiObgkg.EZUQzZsblADVKBE1ZcbbleaRwcyd_KmsUJMwDPWU5GIg.JPEG.mki112112/%EC%8B%A0%EC%A0%95%ED%99%983.jpg?type=w800"},
+            {name:"고영욱", message:"널 좋아해",img: "https://dimg.donga.com/wps/NEWS/IMAGE/2013/01/14/52293938.2.jpg"},
+          {name:"신정환", message:"역시 형이야",img:  "https://mblogthumb-phinf.pstatic.net/MjAxNzA0MjdfMjgx/MDAxNDkzMjYxNzQyMjU4.op0zQTfA81ih-qf3BEzr_6C2VXoclL_Cbs4aiJiObgkg.EZUQzZsblADVKBE1ZcbbleaRwcyd_KmsUJMwDPWU5GIg.JPEG.mki112112/%EC%8B%A0%EC%A0%95%ED%99%983.jpg?type=w800"},
+        ]}
+          renderItem={({item}) =>  <TouchableOpacity style={style.itemView} onPress={()=>{alert(item.name);}}>
+                <Image source={{uri: item.img}} style={style.itemImg}></Image>
+                <View style={{flexDirection:'column'}}>
+                    <Text style={style.itemName}>{item.name}</Text>
+                    <Text style={style.itemMsg}>{item.message}</Text>
+                </View>
+            </TouchableOpacity>}
+                
+     />
     </View>
   );
-};
+}
+
+export default FlatListBasics;
+
+// export default ({ navigation }) => {
+//   useEffect(() => {
+//     navigation.setParams({ area: '동작구' });
+//     console.log('filter:', navigation);
+//   }, []);
+
+//   useLayoutEffect(() => {
+//     navigation.setOptions({
+//       headerTitle: '동작구',
+//       headerRight: () => {
+//         return (
+//           <Header>
+//             <HeaderLink
+//               str={'필터'}
+//               onPress={() => navigation.navigate('Filter')}
+//             />
+//             <HeaderLink
+//               str={'지도'}
+//               onPress={() => navigation.navigate('Map')}
+//             />
+//           </Header>
+//         );
+//       },
+//     });
+//   }, [navigation]);
+
+//   // return (
+//   //   <View>
+//   //     <Text>Home Bitches!!</Text>
+//   //   </View>
+//   // );
+// };
